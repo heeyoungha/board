2941fe5f9b9701dd1ee293808e70787b754fe548
hy-HA <heeyoung-ha@naver.com>
2023-07-21 22:08:06 +0900

    TODO : Audit 작업중

---
+++ src/main/java/board/AuditRevision.java
@@ -0,0 +1,34 @@
+package board;
+
+//@NoArgsConstructor
+//@Getter
+//@Entity
+//@RevisionEntity(AuditRevisionListener.class)
+//@Table(name = "revision")
+//public class AuditRevision {
+//
+//    @RevisionNumber
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "rev")
+//    private Long id;
+//
+//    @RevisionTimestamp
+//    @Column(name = "revstamp")
+//    private Long timestamp;
+//
+//    @Column(name = "admin_id")
+//    private Long adminId;
+//
+//    @Column(name = "operation")
+//    private String operation;
+//
+//    public void putAdminId(Long adminId) {
+//        this.adminId = adminId;
+//    }
+//
+//    public void putOperation(String operation) {
+//        this.operation = operation;
+//    }
+//
+//}
