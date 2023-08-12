2b80fce6fc9f7fa217dee3c589950716dfc8bdf3
hy-HA <heeyoung-ha@naver.com>
2023-08-12 20:28:11 +0900

    fix : MemberRequest의 inner클래스가 2개일때

--- src/main/java/board/board/domain/User.java
+++ src/main/java/board/board/domain/User.java
@@ -2,6 +2,7 @@ package board.board.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.util.Pair;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,4 +20,14 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Reply> replyList;
+
+    public Pair<Long, List<Reply>> get() {
+        Pair<Long, List<Reply>> pair = Pair.of(id, replyList);
+
+        Long id = pair.getFirst();
+        List<Reply> reply = pair.getSecond();
+
+        return pair;
+    }
+
 }
