74787f8925ab91f4683c476e4e18c8b8c3ad0761
hy-HA <heeyoung-ha@naver.com>
2023-08-12 22:15:57 +0900

    docs : 스웨거 업데이트 - 북마크 조회 api - 응답 api

--- src/main/java/board/member/MemberResponse.java
+++ src/main/java/board/member/MemberResponse.java
@@ -10,12 +10,19 @@ import lombok.NoArgsConstructor;
 @Schema(description = "회원가입 응답 폼")
 public class MemberResponse {
 
+    @Schema(description = "아이디", required = true, example = "1")
     private Long id;
+    @Schema(description = "유저네임", required = true, example = "홍길동")
     private String username;
+    @Schema(description = "비밀번호", required = true, example = "1234")
     private String pw;
+    @Schema(description = "나이", example = "20")
     private int age;
+    @Schema(description = "취미", example = "런닝")
     private String interest;
+    @Schema(description = "주소1", example = "서울시 동작구")
     private String address1;
+    @Schema(description = "주소2", example = "상세주소")
     private String address2;
 
     @Builder
